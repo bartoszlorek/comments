c4dcc04c497d212794aa468b27c8742e23615410
Bartosz Lorek <bery.lorek@gmail.com>
2017-07-02 20:09:21 +0200

    introduce mongodb

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var path = require('path');
 var app = express();
 
@@ -10,4 +11,12 @@ app.get('*', (req, res) => {
 
 app.listen(8080, function () {
     console.log('http://localhost:8080/');
+});
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('mongodb is running!')
 });
