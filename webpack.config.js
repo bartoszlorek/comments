6e86d75c6074d9034babf881fb34f8d3e7de966c
Bartosz Lorek <bery.lorek@gmail.com>
2017-07-02 04:24:43 +0200

    separate production and development config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var config = {
+module.exports = {
     entry: [
         'babel-polyfill',
         './src/index.js',
     ],
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'script.js'
     },
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loaders: ['react-hot-loader', 'babel-loader']
+                loader: 'babel-loader'
             }, {
                 test: /\.css$/,
                 loader: 'style-loader',
@@ -29,39 +29,13 @@ var config = {
                     localIdentName: '[name]-[local]--[hash:base64:5]'
                 }
             }
-        ],
+        ]
     },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    }
-}
-
-if (process.env.NODE_ENV === 'production') {
-    config.plugins = [
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-                screw_ie8: true,
-                drop_console: true,
-                drop_debugger: true
-            }
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        })
-    ]
-
-} else {
-    config.devtool = 'cheap-module-source-map';
-    config.devServer = {
+    devtool: 'cheap-module-source-map',
+    devServer: {
         historyApiFallback: true
-    }
-    config.plugins = [
+    },
+    plugins: [
         new webpack.HotModuleReplacementPlugin()
     ]
 }
-
-module.exports = config;
