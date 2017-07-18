a7ce72fb06d675e1d9aa39ac8346753722e77b04
Bartosz Lorek <bery.lorek@gmail.com>
2017-07-18 15:16:40 +0200

    callbacks as object properties instead of multiple variables

--- src/back/utils/result.js
+++ src/back/utils/result.js
@@ -2,21 +2,21 @@ var toOutput = require('./toOutput');
 var format = require('./format');
 var filter = require('./filter');
 
-module.exports = function (response, onSuccess, onError, onFailure) {
+module.exports = function (response, callback = {}) {
     return function (error, data) {
         if (error) {
             if (error) {
                 error = error.message;
             }
-            error = filter(error, onError);
+            error = filter(error, callback.error);
             response.json(format.error(error));
 
         } else if (!data) {
-            data = filter(data, onFailure);
+            data = filter(data, callback.fail);
             response.json(format.error(data));
 
         } else {
-            data = filter(data, onSuccess);
+            data = filter(data, callback.success);
             if (typeof data === 'undefined') {
                 return;
             }
